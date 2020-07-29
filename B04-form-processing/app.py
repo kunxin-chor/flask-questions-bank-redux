237e5a913707c5b162508d2aca821765e5c42641
kunxin-chor <chorkunxin@yahoo.com>
2020-07-29 03:17:11 +0000

    Q4 solutions

--- B04-form-processing/app.py
+++ B04-form-processing/app.py
@@ -3,14 +3,49 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return render_template('home.template.html')
+def ask_for_number():
+    return render_template('get_number_form.template.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.template.html')
+@app.route('/', methods=["POST"])
+def process_number():
+    number = int(request.form.get('number'))
+    return render_template('show_number_pic.template.html', number=number)
+
+
+@app.route('/contact-us')
+def show_contact_form():
+    return render_template('form.template.html')
+
+
+@app.route('/contact-us', methods=["POST"])
+def process_contact_form():
+    if 'democheckbox' in request.form:
+        accept = True
+    else:
+        accept = False
+
+    name = request.form.get('name')
+    gender = request.form.get('sex')
+    comment = request.form.get('comment')
+    can_contact = True if request.form.get('can-contact') == "yes" else False
+
+    # alternate method:
+    # accept = request.form.get('democheckbox')
+    # if accept == 'yes':
+    #     accept = True
+    # else:
+    #     accept = False
+
+    return render_template('display_results.template.html', context={
+        "name": name,
+        "gender": gender,
+        "comment": comment,
+        "can_contact": can_contact,
+        "accept": accept
+    })
 
 
 # "magic code" -- boilerplate
--- B04-form-processing/readme.md
+++ B04-form-processing/readme.md
@@ -19,3 +19,44 @@ Display the form shown in `form.template.html`
 Extract out the name, gender, whether we can contact the user, comments and whether the user has accepted the terms and conditions. 
 Display out the user's input in an unordered list.
 
+# Possible Solutions for Part 1
+
+## METHOD 1:
+```
+@app.route('/', methods=["POST"])
+def process_number():
+    number = int(request.form.get('number'))
+    image = ""
+    if number == 1:
+        image="1.jpg"
+    if number == 2:
+        image="2.jpg"
+    if number == 3:
+        image="3.jpg"
+    return render_template('show_number_pic.template.html', image=image)
+```
+
+## METHOD 2:
+```
+@app.route('/', methods=["POST"])
+def process_number():
+    number = int(request.form.get('number'))
+    return render_template('show_number_pic.template.html', number=number)
+```
+
+```
+<body>
+	{% if number == 1 %}
+	<img src="/static/1.jpg"/>
+    {% elif number == 2 %}
+	<img src="/static/2.jpg"/>
+    {% elif number == 3 %}
+	<img src="/static/3.jpg"/>
+    {% else %}
+    <p>Invalid number</p>
+    {% endif %}
+</body>
+```
+
+## Method 3
+Please see the answer
